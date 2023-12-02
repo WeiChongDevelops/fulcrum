d6f92da9a71ace22dd569e8280601577437536e1
Wei <wac.weichong@gmail.com>
2023-12-03 03:09:00 +0800

    basic react router set up

--- Fulcrum React/src/components/App.tsx
+++ Fulcrum React/src/components/App.tsx
@@ -6,6 +6,10 @@ import {ExpenseItemEntity, getExpenseList} from "../util.ts";
 import DBInsertionForm from "./DBInsertionForm.tsx";
 import DBDeletionForm from "./DBDeletionForm.tsx";
 import DBUpdatingForm from "./DBUpdatingForm.tsx";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FulcrumApp from "./FulcrumApp.tsx";
+import Signup from "./Signup.tsx";
+import Login from "./Login.tsx";
 
 export default function App() {
 
@@ -21,12 +25,18 @@ export default function App() {
     }, [])
 
     return (
-        <>
-            <ExpenseList expenseArray={expenseArray}/>
-            {/*<BudgetList budgetArray={budgetArray} setBudgetArray={setBudgetArray}/>*/}
-            <DBInsertionForm setExpenseArray={setExpenseArray}/>
-            <DBDeletionForm/>
-            <DBUpdatingForm/>
-        </>
+        <Router>
+            <Switch>
+                <Route path="/login" Component={Login} />
+                <Route path="/signup" Component={Signup} />
+                <Route path="/" Component={FulcrumApp} />
+                <ExpenseList expenseArray={expenseArray}/>
+                {/*<BudgetList budgetArray={budgetArray} setBudgetArray={setBudgetArray}/>*/}
+                <DBInsertionForm setExpenseArray={setExpenseArray}/>
+                <DBDeletionForm/>
+                <DBUpdatingForm/>
+            </Switch>
+        </Router>
     )
+}
 }
