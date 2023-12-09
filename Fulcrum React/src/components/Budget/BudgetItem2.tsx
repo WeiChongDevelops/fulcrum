b67008adcf49effd5ad060f10be38a45d539db39
Wei <wac.weichong@gmail.com>
2023-12-09 10:35:59 +0800

    edit and delete buttons added, no functionality

--- Fulcrum React/src/components/Budget/BudgetItem2.tsx
+++ Fulcrum React/src/components/Budget/BudgetItem2.tsx
@@ -7,13 +7,13 @@ interface BudgetItem2Props {
 
 export default function BudgetItem2({category, amount, icon}: BudgetItem2Props) {
     const styles = {
-        boxShadow: "8px 8px 0px 0px rgba(0, 0, 0, 1)" // 100% opacity black shadow
+        boxShadow: "8px 8px 0px 0px rgba(0, 0, 0, 1)"
     }
 
     return (
         <div className="flex flex-row justify-between items-center bg-blue-200 py-1.5 px-2.5 rounded-3xl my-3 text-black"
              style={styles}>
-            <div className="flex flex-row items-start">
+            <div className="flex flex-row items-center">
                 <div className="rounded-full bg-green-950 p-2">
                     <img src={icon} alt=""/>
                 </div>
@@ -22,7 +22,13 @@ export default function BudgetItem2({category, amount, icon}: BudgetItem2Props) 
                     <h3>Parent Category Filler</h3>
                 </div>
             </div>
-            <h2>${amount}</h2>
+            <div className="flex flex-row items-center">
+                <b>${amount}</b>
+                <div className="flex flex-row items-center ml-1.5">
+                    <img src="/src/assets/icons/edit-pencil-icon.svg" alt="" className="ml-2 w-6"/>
+                    <img src="/src/assets/icons/delete-trash-icon.svg" alt="" className="ml-2 w-6"/>
+                </div>
+            </div>
         </div>
     )
 }
