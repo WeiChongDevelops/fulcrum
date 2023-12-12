a2220cf74b1b6f5f98d5d409769cbd3232ab2a6c
Wei <wac.weichong@gmail.com>
2023-12-12 12:02:46 +0800

    added comma formatting to budget number display

--- Fulcrum React/src/components/Budget/BudgetItem.tsx
+++ Fulcrum React/src/components/Budget/BudgetItem.tsx
@@ -32,6 +32,14 @@ export default function BudgetItem({ category, amount, icon, group, setBudgetArr
         boxShadow: "8px 8px 0px 0px rgba(0, 0, 0, 1)"
     };
 
+    const formatNumberWithCommas = (numberString) => {
+        return numberString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
+    const amountDisplay = typeof amount === 'number'
+        ? formatNumberWithCommas(amount.toFixed(2)).toUpperCase()
+        : formatNumberWithCommas(parseFloat(amount).toFixed(2)).toUpperCase();
+
 
     return (
         <div className="flex flex-row justify-between items-center bg-blue-200 py-1.5 px-2.5 rounded-3xl my-3 text-black" style={boxShadowStyle}>
@@ -45,7 +53,7 @@ export default function BudgetItem({ category, amount, icon, group, setBudgetArr
                 </div>
             </div>
             <div className="flex flex-row items-center">
-                <b>${typeof amount === 'number' ? amount.toFixed(2) : parseInt(amount).toFixed(2)}</b>
+                <b>${amountDisplay}</b>
                 <div className="flex flex-row items-center ml-2">
                     <div className="circle-button rounded-full p-1" onClick={handleEditClick}>
                         <img src="/src/assets/UI-icons/edit-pencil-icon.svg" alt="" className="mx-1 w-6 h-6" />
