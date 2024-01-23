6a5f3304cf7ff00ef36b121c9c9595da863e9c96
Wei <wac.weichong@gmail.com>
2024-01-23 18:20:03 +0800

    cleanup

--- Fulcrum React/src/components/App.tsx
+++ Fulcrum React/src/components/App.tsx
@@ -2,7 +2,6 @@ import '../css/App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from "./Auth/Register.tsx";
 import Login from "./Auth/Login.tsx";
-import TestExpenses from "./ExpensesAPI/TestExpenses.tsx";
 import Budget from "./Budget/Budget.tsx";
 import Navbar from "./Other/Navbar.tsx";
 import Expense from "./Expenses/Expense.tsx";
@@ -15,7 +14,6 @@ export default function App() {
                 <Route path="/" element={<Navbar/>} >
                     <Route path="login" element={<Login/>} />
                     <Route path="register" element={<Register/>} />
-                    <Route path="expenses-test" element={<TestExpenses/>} />
                     <Route path="expenses" element={<Expense/>} />
                     <Route path="budget" element={<Budget/>} />
                 </Route>
