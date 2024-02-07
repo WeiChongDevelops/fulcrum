6661c53894d5e25e41a2b5b92f6c7f217e7ae123
Wei <wac.weichong@gmail.com>
2024-02-08 02:30:40 +0800

    adjusted tools page styling for smaller viewport

--- Fulcrum React/src/components/Tools/Tools.tsx
+++ Fulcrum React/src/components/Tools/Tools.tsx
@@ -27,20 +27,20 @@ export default function Tools() {
                 <FulcrumButton displayText={"Sign Out"} backgroundColour={"white"} onClick={logoutOnClick}/>
             </div>
 
-             <div className="tools-tile-container w-full h-auto flex justify-around items-center mt-16">
-                <div className="tools-tile bg-[#D1B1B1] text-black text-4xl hover:cursor-pointer" onClick={openSettings}>
+             <div className="tools-tile-container w-full h-auto flex justify-around items-center mt-10">
+                <div className="tools-tile bg-[#D1B1B1] text-black text-3xl hover:cursor-pointer" onClick={openSettings}>
                     <div className="tools-text-container">
                         <p>Settings</p>
                     </div>
                     <img src="/src/assets/UI-icons/tools-settings-icon-black.svg" alt=""/>
                 </div>
-                <div className="tools-tile bg-[#B1D1CF] text-black text-xl leading-5 hover:cursor-pointer" onClick={openRecurringExpenses}>
+                <div className="tools-tile bg-[#B1D1CF] text-black text-lg leading-5 hover:cursor-pointer" onClick={openRecurringExpenses}>
                     <div className="tools-text-container">
                         <p>Recurring Expenses</p>
                     </div>
                     <img src="/src/assets/UI-icons/tools-recurring-icon-black.svg" alt=""/>
                 </div>
-                <div className="tools-tile bg-[#B1C5D1] text-black text-2xl leading-7">
+                <div className="tools-tile bg-[#B1C5D1] text-black text-xl leading-7">
                     <div className="tools-text-container">
                         <p>Coming Soon</p>
                     </div>
