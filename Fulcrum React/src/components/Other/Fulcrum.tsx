22b855a2458abde82062a14bae115658eb2ed1f3
Wei <wac.weichong@gmail.com>
2024-03-02 18:07:22 +0800

    adjusted gitignore, refactored both navbars to dedicated components for SOC

--- Fulcrum React/src/components/Other/Fulcrum.tsx
+++ Fulcrum React/src/components/Other/Fulcrum.tsx
@@ -1,8 +1,8 @@
-import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import {Dispatch, SetStateAction, useEffect} from "react";
 import {Outlet} from "react-router-dom";
-import {getSessionEmail, getWindowLocation, logoutOnClick, PublicUserData} from "../../util.ts";
-import FulcrumButton from "./FulcrumButton.tsx";
-import DarkModeToggle from "./DarkModeToggle.tsx";
+import {getSessionEmail, PublicUserData} from "../../util.ts";
+import NavbarUpper from "./NavbarUpper.tsx";
+import NavbarLower from "./NavbarLower.tsx";
 
 interface FulcrumProps {
     publicUserData: PublicUserData;
@@ -12,7 +12,6 @@ interface FulcrumProps {
 }
 
 export default function Fulcrum({ publicUserData, setPublicUserData, email, setEmail }: FulcrumProps) {
-    const [hoveredNavButton, setHoveredNavButton] = useState("");
 
     useEffect(() => {
         getSessionEmail()
@@ -23,81 +22,10 @@ export default function Fulcrum({ publicUserData, setPublicUserData, email, setE
         sessionStorage.setItem("email", email);
     }, [email]);
 
-    function handleMouseEnter(e: React.MouseEvent<HTMLButtonElement>) {
-        const button = e.target as HTMLButtonElement;
-        setHoveredNavButton(button.id);
-    }
-
-    function handleMouseLeave() {
-        setHoveredNavButton("");
-    }
-
     return (
         <div className={`transition-filter duration-500 ease-in-out min-h-screen ${publicUserData.accessibilityEnabled && "accessibility-enabled"}`}>
-            <nav className={`flex flex-row flex-shrink justify-between items-center h-[55px] py-1 ${publicUserData.darkModeEnabled ?  "bg-dark" : "bg-light"}`}>
-                <div className="flex-1 ml-10">
-                    <DarkModeToggle publicUserData={publicUserData} setPublicUserData={setPublicUserData}/>
-                </div>
-                <img src={`/src/assets/fulcrum-logos/fulcrum-long-${publicUserData.darkModeEnabled ? "white" : "black"}.webp`} alt="Fulcrum logo in navbar"
-                     className="navbar-fulcrum-logo mr-12  select-none hover:cursor-pointer "
-                     onClick={() => window.location.href = "/budget"}/>
-                <div className="flex-1 text-right">
-                    <div className="flex justify-end items-center mr-8">
-                        <p className={`navbar-email select-none ${publicUserData.darkModeEnabled ? "text-white": "text-black"}`}>{email}</p>
-                        <img
-                            src={`/src/assets/profile-icons/${publicUserData.profileIconFileName.slice(0, -4)}-${publicUserData.darkModeEnabled ? "white" : "black"}.svg`}
-                            className="profile-icon h-12" alt="Profile icon"/>
-                        {email != "" ? <FulcrumButton displayText="Log Out" onClick={logoutOnClick}/>
-                            : <FulcrumButton displayText="Register"
-                                             onClick={() => window.location.href = "/register"}/>}
-                    </div>
-                </div>
-            </nav>
-            <nav className="text-white font-bold z-10">
-                <div className="flex flex-row justify-center items-center">
-                    <div className="flex flex-1 justify-around border-4 border-black bg-black">
-                        <button id="expenses" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
-                                className={`flex flex-row justify-center items-center w-[33.33%] rounded-xl font-bold py-4 ${getWindowLocation() === "expenses" ? "bg-[#17423F]" : "bg-black"} border-2 border-black hover:bg-[#efefef] hover:text-black transition-colors duration-300 ease-in-out`}
-                                onClick={() => window.location.href = "/expenses"}>
-                            <img
-                                src={`/src/assets/navbar-icons/expenses-icon-${getWindowLocation() === "expenses" ? "white" : "black"}.svg`}
-                                className={`w-6 transition-opacity duration-300 ${(getWindowLocation() === "expenses" && hoveredNavButton === "expenses") && "opacity-0"}`}
-                                alt=""/>
-                            <p className="mx-4">Expenses</p>
-                            <img
-                                src={`/src/assets/navbar-icons/expenses-icon-${getWindowLocation() === "expenses" ? "white" : "black"}.svg`}
-                                className={`w-6 transition-opacity duration-300 ${(getWindowLocation() === "expenses" && hoveredNavButton === "expenses") && "opacity-0"}`}
-                                alt=""/>
-                        </button>
-                        <button id="budget" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
-                                className={`flex flex-row justify-center items-center w-[33.33%] rounded-xl font-bold py-4 ${getWindowLocation() === "budget" ? "bg-[#17423F]" : "bg-black"} border-y-2 border-x-4 border-black hover:bg-[#efefef] hover:text-black transition-colors duration-300 ease-in-out`}
-                                onClick={() => window.location.href = "/budget"}>
-                            <img
-                                src={`/src/assets/navbar-icons/budget-icon-${getWindowLocation() === "budget" ? "white" : "black"}.svg`}
-                                className={`w-6 transition-opacity duration-300 ${(getWindowLocation() === "budget" && hoveredNavButton === "budget") && "opacity-0"}`}
-                                alt=""/>
-                            <p className="mx-4">Budget</p>
-                            <img
-                                src={`/src/assets/navbar-icons/budget-icon-${getWindowLocation() === "budget" ? "white" : "black"}.svg`}
-                                className={`w-6 transition-opacity duration-300 ${(getWindowLocation() === "budget" && hoveredNavButton === "budget") && "opacity-0"}`}
-                                alt=""/>
-                        </button>
-                        <button id="tools" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
-                                className={`flex flex-row justify-center items-center w-[33.33%] rounded-xl font-bold py-4 ${getWindowLocation() === "tools" ? "bg-[#17423F]" : "bg-black"} border-2 border-black hover:bg-[#efefef] hover:text-black transition-colors duration-300 ease-in-out`}
-                                onClick={() => window.location.href = "/tools"}>
-                            <img
-                                src={`/src/assets/navbar-icons/tools-icon-${getWindowLocation() === "tools" ? "white" : "black"}.svg`}
-                                className={`w-6 transition-opacity duration-300 ${(getWindowLocation() === "tools" && hoveredNavButton === "tools") && "opacity-0"}`}
-                                alt=""/>
-                            <p className="mx-4">Tools</p>
-                            <img
-                                src={`/src/assets/navbar-icons/tools-icon-${getWindowLocation() === "tools" ? "white" : "black"}.svg`}
-                                className={`w-6 transition-opacity duration-300 ${(getWindowLocation() === "tools" && hoveredNavButton === "tools") && "opacity-0"}`}
-                                alt=""/>
-                        </button>
-                    </div>
-                </div>
-            </nav>
+            <NavbarUpper publicUserData={publicUserData} setPublicUserData={setPublicUserData} email={email}/>
+            <NavbarLower/>
             {!window.location.href.includes("tools") && <div id="background-grid" className={`background ${publicUserData.darkModeEnabled ? "bg-dark" : "bg-light"}`}></div>}
             <Outlet/>
         </div>
