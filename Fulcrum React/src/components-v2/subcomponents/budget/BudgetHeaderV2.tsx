26cc4a56781c1fedc22f6bee1036cdcf9d36d6fe
Wei <wac.weichong@gmail.com>
2024-05-25 08:33:00 +0800

    adjust app header styles

--- Fulcrum React/src/components-v2/subcomponents/budget/BudgetHeaderV2.tsx
+++ Fulcrum React/src/components-v2/subcomponents/budget/BudgetHeaderV2.tsx
@@ -15,10 +15,25 @@ export default function BudgetHeaderV2({ publicUserData, totalIncome, navMenuOpe
       className={`fixed flex flex-row z-40 gap-4 justify-start items-center self-end bg-gray-400 ${navMenuOpen ? "w-[calc(100vw-14rem)]" : "w-[calc(100vw-5rem)]"} h-[6vh]`}
     >
       <MonthlyIncomeV2 publicUserData={publicUserData} className={"ml-10"} totalIncome={totalIncome} />
-      <div className={"flex flex-row justify-center items-center gap-4 ml-auto mr-2"}>
-        <p className={"mx-8 font-medium text-lg"}>Budget</p>
+      <div className={"flex flex-row justify-center items-center gap-3 ml-auto mr-2"}>
+        <p className={"mr-3 font-medium text-base"}>Budget</p>
         <DarkModeToggleV2 publicUserData={publicUserData} />
-        <Button>Help</Button>
+        <Button variant={"ghost"} className={"p-2 mr-2"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.8}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+            />
+          </svg>
+        </Button>
       </div>
     </div>
   );
--- Fulcrum React/src/components-v2/subcomponents/settings/SettingsHeaderV2.tsx
+++ Fulcrum React/src/components-v2/subcomponents/settings/SettingsHeaderV2.tsx
@@ -13,10 +13,25 @@ export default function SettingsHeaderV2({ publicUserData }: SettingsHeaderV2) {
     <div
       className={`fixed flex flex-row z-40 gap-4 justify-start items-center self-end bg-gray-400 ${navMenuOpen ? "w-[calc(100vw-14rem)]" : "w-[calc(100vw-5rem)]"} h-[6vh]`}
     >
-      <div className={"flex flex-row justify-center items-center gap-4 ml-auto mr-2"}>
-        <p className={"mx-8 font-bold text-xl"}>Settings</p>
+      <div className={"flex flex-row justify-center items-center gap-3 ml-auto mr-2"}>
+        <p className={"mr-3 font-medium text-base"}>Settings</p>
         <DarkModeToggleV2 publicUserData={publicUserData} />
-        <Button>Help</Button>
+        <Button variant={"ghost"} className={"p-2 mr-2"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.8}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+            />
+          </svg>
+        </Button>
       </div>
     </div>
   );
